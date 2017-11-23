9e319c65b6edbfdcaed4ddd0b8930faba6f8a95a
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2017-11-23 16:07:59 -0600

    event handling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class App extends React.Component{
-      
+  constructor() {
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    this.setState({currentEvent: e.type})
+  }
   render() {
-
-    return <Title text="acbdef"/>
+    return (
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+          cols="30" 
+          rows="10" />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
   }
 }
 
-const Title = (props) => {
-  return <h1> Title: {props.text} </h1>
-}
 
-Title.propTypes = {
-  text(props, propName, componentName) {
-    if(!(propName in props)) {
-      return new Error(`missing ${propName}`)
-    }
-
-    if(props[propName].length < 6 ) {
-      return new Error (`${propName} was too short`)
-    }
-  }
-}
 
 
 export default App;
