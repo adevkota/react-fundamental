e141c41a6e591877c130a5de35bcca8ae55ed732
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2017-11-24 08:58:12 -0600

    array of components using map

--- src/App.js
+++ src/App.js
@@ -1,77 +1,40 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 // import PropTypes from 'prop-types';
 
 class App extends React.Component{
   constructor() {
     super();
-    this.state = {increasing: false};
-    this.update = this.update.bind(this);
+    this.state = {items: []};
+    // this.update = this.update.bind(this);
   }
 
-  update() {
-    ReactDOM.render(
-      <App val={this.props.val+1} />,
-      document.getElementById('root')
+  componentWillMount() {
+    fetch("https://swapi.co/api/people/?format=json")
+      .then(response => response.json())
+      .then(({results: items}) => this.setState({items}))
+  }
+  filter(e) {
+    this.setState({filter: e.target.value})
+  }
+  render() {
+    let items = this.state.items
+    if(this.state.filter) {
+      items = items.filter( item => 
+        item.name.toLowerCase()
+        .includes(this.state.filter.toLowerCase())
+      )
+    }
+    return (
+      <div>
+        <input type="text" onChange={this.filter.bind(this)}/>
+        {items.map(item => 
+          <Person key={item.name} person={item} />
+        )}
+      </div>
     )
   }
-
-  
-  // componentWillMount() {
-  //   console.log("componentWillMount");
-  //   this.setState({m:2})
-  // }
-  componentWillReceiveProps(nextProps) {
-    this.setState({increasing: nextProps.val > this.props.val})
-  }
-  
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
-  }
-  
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`)
-  }
-  
-  render() {
-    console.log(this.state.increasing)
-    return <button onClick={this.update}>
-      {this.props.val}</button>
-  }
-  
-  // componentDidMount() {
-  //   console.log("componentDidMount");
-  //   console.log(ReactDOM.findDOMNode(this));
-  //   this.inc = setInterval(this.update, 500);
-  // }
-
-  // componentWillUnmount() {
-  //   console.log("componentWillUnmount");
-  //   clearInterval(this.inc);
-  // }
 }
 
-// class Wrapper extends React.Component {
-//   mount() {
-//     ReactDOM.render(<App />,
-//       document.getElementById('a'))
-//   }
-
-//   unMount() {
-//     ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.mount.bind(this)}>Mount</button>
-//         <button onClick={this.unMount.bind(this)}>UnMount</button>
-//         <div id="a"></div>
-//       </div>
-//     )
-//   }
-// }
-
-App.defaultProps = {
-  val: 0
-}
+const Person = (props) => <h4>{props.person.name}</h4>
 export default App;
