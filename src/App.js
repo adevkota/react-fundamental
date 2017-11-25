c5ee57aafa7de3cc096498c57cca25ceecc74d21
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2017-11-25 13:04:48 -0600

    resubale components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,97 @@
 import React from "react";
-import './App.css';
-// import ReactDOM from "react-dom";
-// import PropTypes from 'prop-types';
+import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
 class App extends React.Component{
-  
-
-  render() {
-    return (
-     <Buttons>
-       <button value="A">A</button>
-       <button value="B">B</button>
-       <button value="C">C</button>
-     </Buttons>
-    )
-  }
-}
-
-class Buttons extends React.Component{
   
   constructor() {
     super();
     this.state = {
-      selected: 'None'
-    }    
-  }
-
-  selectItem(selected) {
-    this.setState({selected})
-  }
-
-  render() {
-    let fn = child => {
-      return React.cloneElement(child, {
-        onClick:this.selectItem.bind(this, child.props.value)
-      })
+      red:0,
+      green:0,
+      blue:0
     }
-    let items = React.Children.map(this.props.children, fn)
+    this.update = this.update.bind(this)   
+  }
+
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+      
+    })
+  }
+  render() {
     return (
       <div>
-        <h2>You have selected: {this.state.selected}</h2>
-        {items}
+
+        <NumInput 
+          ref="red"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.red}
+          label="Red"
+          update={this.update} />
+        <NumInput 
+          ref="green"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.green}
+          label="Green"
+          update={this.update} />
+        <NumInput 
+          ref="blue"
+          min={0}
+          max={255}
+          step={1}
+          val={+this.state.blue}
+          label="Blue"
+          update={this.update} />
+      </div>
+
+    )
+  }
+}
+
+
+class NumInput extends React.Component {
+  render() {
+    let label = this.props.label !== '' ?
+      <label>{this.props.label} - {this.props.val}</label>:''
+    return (
+      <div>
+        <input ref="inp" 
+         type={this.props.type}
+         min={this.props.min}
+         max={this.props.max}
+         step={this.props.step}
+         defaultValue={this.props.val}
+         onChange={this.props.update}/>
+         {label}
       </div>
     )
   }
+}
+
+NumInput.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
+  val: PropTypes.number,
+  label: PropTypes.string,
+  update: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(['number', 'range'])
+}
+
+NumInput.defaultProps = {
+  min:0,
+  max:0,
+  range:1,
+  val:0,
+  label: '',
+  type: 'range'
 }
 export default App;
 
